f4f23f20cb542c9eb43a2db168fcbcd8ed0f2036
len739 <glennjoshua69@gmail.com>
2025-05-27 09:36:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,15 @@ import streamlit as st
 
 st.title("🎈 halo lino ganteng")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+   "Ngoding seru bersama Rafalino
+   )
+st.image("17483129038273881632111553702091.jpg",width=200)
+
+st.title("aplikasi sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganji")
